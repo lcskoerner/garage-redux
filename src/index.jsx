f29220a6137d1d4cf1a2173c94ab3650d920c9dd
Lucas Koerner <lcskoerner@gmail.com>
2020-11-02 18:48:43 -0500

    initial state and carsReducer

--- src/index.jsx
+++ src/index.jsx
@@ -9,15 +9,32 @@ import { createHistory as history } from 'history';
 
 import '../assets/stylesheets/application.scss';
 
+import carsReducer from './reducers/cars_reducer';
+
+const garageName = 'Jojo\'s Repair Shop';
+const cars = [
+  { id: 1, brand: 'Peugeot', model: '106', owner: 'John', plate: 'WOB-ED-42' },
+  { id: 2, brand: 'Renault', model: 'Scenic', owner: 'Paul', plate: 'AAA-12-BC' },
+  { id: 3, brand: 'Aston Martin', model: 'DB Mark III', owner: 'James', plate: '418-ED-94' },
+  { id: 4, brand: 'VW', model: 'Beetle', owner: 'George', plate: '1234-XD-75' }
+];
+
+const initialState = {
+  garage: garageName,
+  cars: cars
+}
+
 const reducers = combineReducers({
-  // key: reducer
+  garage: (state = null, action) => state,
+  cars: carsReducer
 });
 
 const middlewares = applyMiddleware(reduxPromise, logger);
+const store = createStore(reducers, initialState, middlewares);
 
-// render an instance of the component in the DOM
 ReactDOM.render(
-  <Provider store={createStore(reducers, {}, middlewares)}>
+  // render an instance of the component in the DOM
+  <Provider store={store}>
     <Router history={history}>
       <Switch>
         TODO
---
+++ src/reducers/cars_reducer.js
@@ -0,0 +1,10 @@
+export default function(state, action) {
+  if (state === undefined) {
+    return state;
+  }
+
+  switch(action.type) {
+    // TODO
+  }
+
+}
